8a3a21973d12c00f10568245c82b91b1e1c51d4c
azola <aizydyantyi@gmail.com>
2016-11-03 16:48:54 +0200

    dynamically_typed.js

---
+++ my_variables.js
@@ -0,0 +1,10 @@
+ var sunshine = 'True';
+ console.log(sunshine);
+ var age = 40;
+ console.log(age);
+ var name = 'azola';
+console.log(name)
+var date = new Date();
+console.log(date);
+var fruit = ['Apple', 'Banana'];
+console.log(fruit);
